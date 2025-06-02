82e1e1c65ae097589975fb449a4b9c052e089621
demi ajala <bidemiajala@gmail.com>
2025-06-02 21:13:34 +0100

    retry analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Analytics />
       {/* Header */}
       <header className="bg-white shadow-sm border-b border-gray-200">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
@@ -79,6 +78,7 @@ function App() {
           </div>
         </div>
       </footer>
+      <Analytics />
     </div>
   );
 }
